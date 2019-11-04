fba807ae2e4328df3b338fdaada958eafcf3513f
Ivy Lin <a0981792089@gmail.com>
2019-11-04 23:42:34 +0800

    修正(建立 migration)

--- database/migrations/2019_11_04_154130_create_posts_table.php
+++ database/migrations/2019_11_04_154130_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTableCreate=posts extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class CreatePostsTableCreate=posts extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class CreatePostsTableCreate=posts extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('posts');
     }
 }

a3b6ef6f5b7f26f97e8029108bac00d46b6584bc
Ivy Lin <a0981792089@gmail.com>
2019-11-05 23:09:03 +0800

    新增資料，使用 create方法，設定 Post.php的 fillable屬性

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+    \App\Post::create([
+        'title'=>'test title',
+        'content'=>'test content',
+    ]);
+
 });
 Route::get('posts', ['as' => 'posts.index', 'uses' =>'PostsController@index']);
 Route::get('post', ['as' => 'posts.show', 'uses' => 'PostsController@show']);
 Route::get('about', ['as' => 'posts.about', 'uses' =>'PostsController@about']);
 Route::get('contact', ['as' => 'posts.contact', 'uses' =>'PostsController@contact']);
+
+

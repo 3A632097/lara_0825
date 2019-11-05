6ffa05c28e48d5642abac5f27cae9fe36e79807f
Ivy Lin <a0981792089@gmail.com>
2019-11-05 23:47:40 +0800

    練習條件式&更新資料使用 update方法

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,32 @@
 
 Route::get('/', function () {
 //    return view('welcome');
+    //使用 create方法
 //    \App\Post::create([
 //        'title'=>'test title',
 //        'content'=>'test content',
 //    ]);
-
+    //改用 new 的方式新增資料
 //    $post=new\App\Post();
 //    $post->title='testtitle';
 //    $post->content='testcontent';
 //    $post->save();
-
+    //使用 all方法
 //    $posts=\App\Post::all();
 //    dd($posts);
-
+    //使用 find 方法
 //    $post=\App\Post::find(1);
 //    dd($post);
-
-    $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($posts);
+    //練習條件式
+//    $posts=\App\Post::where('id','<',10)->orderBy('id','DESC')->get();
+//    dd($posts);
+    //更新資料
+    //使用 update方法
+    $post=\App\Post::find(1);
+    $post->update([
+        'title'=>'updated title',
+        'content'=>'updated content',
+    ]);
 });
 Route::get('posts', ['as' => 'posts.index', 'uses' =>'PostsController@index']);
 Route::get('post', ['as' => 'posts.show', 'uses' => 'PostsController@show']);

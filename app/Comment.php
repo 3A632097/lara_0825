2d508f5003b834ced25adb3de0b7d392c636069c
Ivy Lin <a0981792089@gmail.com>
2019-12-28 12:50:14 +0800

    開啟 Comment.php，增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App;
-
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
     protected $fillable = ['content'];
+    
+    public function post()
+    {
+        return $this->belongTo(Post::class);
+    }
 }
